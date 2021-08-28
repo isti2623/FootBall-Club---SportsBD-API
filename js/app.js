1ebd6df67e253919b3b25e855f3392cb1fdc2728
isti2623 <istiaqueahmed2019.bd@gmail.com>
2021-08-28 14:02:06 +0600

    update

--- js/app.js
+++ js/app.js
@@ -14,12 +14,12 @@ const showData = team => {
     const showCard = document.getElementById("show-card");
     showCard.textContent = '';
     team.forEach(element => {
-        console.log(element);
+        // console.log(element);
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-        <div class="card h-100">
-        <img class="w-25" src="${element.strTeamBadge}" class="card-img-top" alt="...">
+        <div onclick="detailsLoad(${element.idTeam})" class="card h-100">
+        <img class="w-50" src="${element.strTeamBadge}" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title">${element.strTeam}</h5>
             <p class="card-text">${element.strDescriptionEN.slice(0, 200)}</p>
@@ -27,6 +27,42 @@ const showData = team => {
     </div>
         `
         showCard.appendChild(div);
+
     });
+}
+
+const detailsLoad = team => {
+
+    const url = `https://www.thesportsdb.com/api/v1/json/1/lookupteam.php?id=${team}
+    `
+    fetch(url)
+        .then(res => res.json())
+        .then(data => showDetails(data.teams[0]))
+}
+
+const showDetails = e => {
+    console.log(e);
+    // window.location.href = "two.html";
+
+    const showTeam = document.getElementById("show-team");
+    showTeam.textContent = '';
+
+    e.forEach(element => {
+        console.log(element);
+        const div = document.createElement('div');
+        div.classList.add('card');
+
+        div.innerHTML = `
+        <img src="${element.strTeamBanner}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${element.strAlternate}</h5>
+                <p class="card-text">${element.strStadiumDescription.slice(0, 300)}</p>
+                <p class="card-text"><small class="text-muted">${element.strYoutube}</small></p>
+            </div>
+        `
+        showTeam.appendChild(div);
+
+    })
+
 }
 
